aef84ca9b0e87e2260b6ac7193bf4aa8e73156ae
Callie Bensel <callie.f.bensel@gmail.com>
2016-12-08 11:16:02 -0600

    pro mode complete

--- public/scripts/genericScript.js
+++ public/scripts/genericScript.js
@@ -78,16 +78,23 @@ $('#sortButton').on('click', function(){
 $(document).on('click', '.athleteName', function(){
   console.log('athlete name clicked');
   var athlete = $(this).data('athlete');
+  console.log('athlete name clicked', athlete);
   var athleteArray = [];
+
+  // empty output
+  $('#eventsOutput').html('');
   for (var i = 0; i < eventsArray.length; i++) {
     if (eventsArray[i].athleteName === athlete) {
       athleteArray.push(eventsArray[i]);
+      console.log( 'match found:', eventsArray[i] );
+      appendEventsToDom(eventsArray[i]);
     } // end if
-    appendEventsToDom(athleteArray[i]); // couldn't figure out how to do Pro mode 
+     // couldn't figure out how to do Pro mode
   } //end for
 }); // end athlete button
 
 var appendEventsToDom = function(array){
+  console.log( 'in appendEventsToDom', array );
   $('#eventsOutput').append('<div class="event"></div>');
   var $el = $('#eventsOutput').children().last();
   $el.append('<p>' + array.eventName + '</p>');
